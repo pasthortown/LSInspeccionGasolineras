a90a3581273f84a37f0fe311474ce586f91ae2d4
pasthortown <pastorchessmaster@gmail.com>
2019-03-18 16:12:20 -0500

    correcto

--- client/src/app/layout/register-client/register-client.module.ts
+++ client/src/app/layout/register-client/register-client.module.ts
@@ -4,14 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterClientRoutingModule } from './register-client-routing.module';
 import { RegisterClientComponent } from './register-client.component';
-
+import { PersonService } from './../../services/CRUD/person.service';
+import { GenderService } from './../../services/CRUD/gender.service';
 @NgModule({
-  imports: [CommonModule, 
+  imports: [CommonModule,
             RegisterClientRoutingModule,
             FormsModule],
   declarations: [RegisterClientComponent],
   providers: [
     NgbModal,
+    PersonService,
+    GenderService,
     ]
 })
 export class RegisterClientModule {}
